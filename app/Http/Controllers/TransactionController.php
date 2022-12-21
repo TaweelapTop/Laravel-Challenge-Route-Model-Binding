010e6a65ae41c604b3af931b542bc2418c285b97
TaweelapTop <117077117+TaweelapTop@users.noreply.github.com>
2022-12-21 12:16:19 +0700

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class TransactionController extends Controller
 {
-    public function index()
+    public function index() 
     {
         $transactions = \App\Models\Transaction::with('user')->get();
 
